10f4f4972c51fb9b7b29d54bd350889ff4ebe2be
Nifeel17 <Nifeel17@gmail.com>
2024-03-07 19:00:27 +0100

    Dodaj pliki projektów.

---
+++ Centrum/Classes/WeatherData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Centrum.Classes
+{
+    public class Location
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+    }
+
+    public class Condition
+    {
+        // Assuming there are properties within the condition object
+        // You should add properties here based on the actual JSON structure of the condition object
+    }
+
+    public class Current
+    {
+        [JsonPropertyName("wind_kph")]
+        public double Wind_kph { get; set; }
+        [JsonPropertyName("pressure_mb")]
+        public double pressure_mb { get; set; }
+        [JsonPropertyName("temp_c")]
+        public double Temp_c { get; set; }
+    }
+
+    public class WeatherData
+    {
+        [JsonPropertyName("location")]
+        public Location Location { get; set; }
+        [JsonPropertyName("current")]
+        public Current Current { get; set; }
+    }
+}
---
+++ Centrum/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Centrum.Classes;
+using System.Text.Json;
+
+namespace Centrum
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadWeatherData();
+        }
+
+        public async void LoadWeatherData()
+        {
+            var weatherData = await GetWeatherAsync();
+            if (weatherData != null)
+            {
+                pogoda.Text = $"{weatherData.Current.Temp_c}";
+            }
+        }
+
+        public async Task<WeatherData> GetWeatherAsync()
+        {
+            var response = await _httpClient.GetAsync($"");
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Failed to fetch weather data: {response.StatusCode}");
+            }
+            var content = await response.Content.ReadAsStringAsync();
+            WeatherData dane = JsonSerializer.Deserialize<WeatherData>(content);
+            //pogoda.Text = content;
+            return dane;
+        }
+    }
+
+
+    
+}
