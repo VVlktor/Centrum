915a88df3b0a702e10f96ac4374396ac0d8c76c3
Nifeel17 <Nifeel17@gmail.com>
2024-03-12 15:53:01 +0100

    Dodanie strony newsa

--- Centrum/Pages/PageOfNews.xaml.cs
+++ Centrum/Pages/PageOfNews.xaml.cs
@@ -4,8 +4,30 @@ namespace Centrum.Pages;
 
 public partial class PageOfNews : ContentPage
 {
-	public PageOfNews()
-	{
+    NewsItem News = new NewsItem();
+    public PageOfNews(NewsItem ReceivedNews)
+    {
+        InitializeComponent();
+        News = ReceivedNews;
+        ShowNewsData();
+    }
 
-	}
+    public void ShowNewsData()
+    {
+        NewsLabelTitle.Text = $"{News.Title}";
+        NewsLabelDate.Text = $"{News.PublishDate}";
+        NewsImage.Source = News.Image;
+        NewsLabelText.Text = $"{News.Text}";
+        string autorzy = "";
+        foreach (var x in News.Authors)
+        {
+            autorzy = autorzy + x + ", ";
+        }
+        if (autorzy.Length >= 2)
+        {
+            autorzy = autorzy.Remove(autorzy.Length - 2);
+        }
+        NewsLabelAuthors.Text = autorzy;
+    }
+
 }
