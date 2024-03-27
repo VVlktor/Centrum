d0cc6997070ffc5ba43248b5959e92aa4d86de59
Nifeel17 <Nifeel17@gmail.com>
2024-03-27 21:30:51 +0100

    Mozliwosc ustawienia innych jezykow nauki w ustawieniach

--- Centrum/Pages/LearningPage.xaml.cs
+++ Centrum/Pages/LearningPage.xaml.cs
@@ -4,13 +4,18 @@ namespace Centrum.Pages;
 
 public partial class LearningPage : ContentPage
 {
-    string[,] Characters = { { "あ", "a" }, { "い", "i" }, { "う", "u" }, { "え", "e" }, { "お", "o" }, { "か", "ka" }, { "き", "ki" }, { "く", "ku" }, { "け", "ke" }, { "こ", "ko" }, { "さ", "sa" }, { "し", "shi" }, { "す", "su" }, { "せ", "se" }, { "そ", "so" }, { "た", "ta" }, { "ち", "chi" }, { "つ", "tsu" }, { "て", "te" }, { "と", "to" }, { "な", "na" }, { "に", "ni" }, { "ぬ", "nu" }, { "ね", "ne" }, { "の", "no" }, { "は", "ha" }, { "ひ", "hi" }, { "ふ", "fu" }, { "へ", "he" }, { "ほ", "ho" }, { "ま", "ma" }, { "み", "mi" }, { "む", "mu" }, { "め", "me" }, { "も", "mo" }, { "や", "ya" }, { "ゆ", "yu" }, { "よ", "yo" }, { "ら", "ra" }, { "り", "ri" }, { "る", "ru" }, { "れ", "re" }, { "ろ", "ro" }, { "わ", "wa" }, { "を", "o" }, { "ん", "n" } };
+    string[,] JapaneseCharacters = { { "あ", "a" }, { "い", "i" }, { "う", "u" }, { "え", "e" }, { "お", "o" }, { "か", "ka" }, { "き", "ki" }, { "く", "ku" }, { "け", "ke" }, { "こ", "ko" }, { "さ", "sa" }, { "し", "shi" }, { "す", "su" }, { "せ", "se" }, { "そ", "so" }, { "た", "ta" }, { "ち", "chi" }, { "つ", "tsu" }, { "て", "te" }, { "と", "to" }, { "な", "na" }, { "に", "ni" }, { "ぬ", "nu" }, { "ね", "ne" }, { "の", "no" }, { "は", "ha" }, { "ひ", "hi" }, { "ふ", "fu" }, { "へ", "he" }, { "ほ", "ho" }, { "ま", "ma" }, { "み", "mi" }, { "む", "mu" }, { "め", "me" }, { "も", "mo" }, { "や", "ya" }, { "ゆ", "yu" }, { "よ", "yo" }, { "ら", "ra" }, { "り", "ri" }, { "る", "ru" }, { "れ", "re" }, { "ろ", "ro" }, { "わ", "wa" }, { "を", "o" }, { "ん", "n" } };
+    string[,] GreekCharacters = { { "α", "alpha" }, { "β", "beta" }, { "γ", "gamma" }, { "δ", "delta" }, { "ε", "epsilon" }, { "ζ", "zeta" }, { "η", "eta" }, { "θ", "theta" }, { "ι", "iota" }, { "κ", "kappa" }, { "λ", "lambda" }, { "μ", "mu" }, { "ν", "nu" }, { "ξ", "xi" }, { "ο", "omicron" }, { "π", "pi" }, { "ρ", "rho" }, { "σ", "sigma" }, { "τ", "tau" }, { "υ", "upsilon" }, { "φ", "phi" }, { "χ", "chi" }, { "ψ", "psi" }, { "ω", "omega" } };
+    string[,] ArabicCharacters = { { "ا", "a" }, { "ب", "b" }, { "ت", "t" }, { "ث", "th" }, { "ج", "j" }, { "ح", "h" }, { "خ", "kh" }, { "د", "d" }, { "ذ", "dh" }, { "ر", "r" }, { "ز", "z" }, { "س", "s" }, { "ش", "sh" }, { "ص", "s" }, { "ض", "d" }, { "ط", "t" }, { "ظ", "dh" }, { "ع", "a" }, { "غ", "gh" }, { "ف", "f" }, { "ق", "q" }, { "ك", "k" }, { "ل", "l" }, { "م", "m" }, { "ن", "n" }, { "ه", "h" }, { "و", "w" }, { "ي", "y" } };
+    string[,] RussianCharacters = { { "а", "a" }, { "б", "b" }, { "в", "v" }, { "г", "g" }, { "д", "d" }, { "е", "e" }, { "ё", "yo" }, { "ж", "zh" }, { "з", "z" }, { "и", "i" }, { "й", "j" }, { "к", "k" }, { "л", "l" }, { "м", "m" }, { "н", "n" }, { "о", "o" }, { "п", "p" }, { "р", "r" }, { "с", "s" }, { "т", "t" }, { "у", "u" }, { "ф", "f" }, { "х", "h" }, { "ц", "ts" }, { "ч", "ch" }, { "ш", "sh" }, { "щ", "sht" }, { "ъ", "j" }, { "ы", "i" }, { "ь", "j" }, { "э", "e" }, { "ю", "yu" }, { "я", "ya" } };
+    string[,] Characters;//wyrzucic to gdzies
     int correctAnswer = 0;
 
     Dictionary<int, Label> przyciskiOdpowiedzi;
 	public LearningPage()
 	{
 		InitializeComponent();
+        SetCharactersArray();
         przyciskiOdpowiedzi = new Dictionary<int, Label>()
         {
             { 1, Odpowiedz1 },
@@ -42,9 +47,29 @@ public partial class LearningPage : ContentPage
         QuestionLabel.Text = $"{Characters[whichAnswer,0]}";
     }
 
+    public void SetCharactersArray()
+    {
+        string lng = Preferences.Default.Get("LearningLanguage", "Japoński");
+        switch (lng)
+        {
+            case "Grecki":
+                Characters = GreekCharacters;
+                break;
+            case "Arabski":
+                Characters = ArabicCharacters;
+                break;
+            case "Rosyjski":
+                Characters = RussianCharacters;
+                break;
+            default:
+                Characters = JapaneseCharacters;
+                break;
+        }
+    }
+
     public void AddTableOfCharacters()
     {
-        for (int i = 0; i < 44; i = i + 4)
+        for (int i = 0; i < Characters.Length/2-4; i = i + 4)
         {
             StackLayout stackRow = new StackLayout() { Orientation = StackOrientation.Horizontal, HorizontalOptions = LayoutOptions.Center };
             for (int j = 0; j < 4; j++)
@@ -86,41 +111,6 @@ public partial class LearningPage : ContentPage
             LayoutOfCharacters.Add(stackRow);
 
         }
-        StackLayout stackLastRow = new StackLayout() { Orientation = StackOrientation.Horizontal, HorizontalOptions = LayoutOptions.Center };
-        for (int i = 44; i < 46; i++)
-        {
-            StackLayout WewnetrznyLayout = new StackLayout();
-
-            Label labelJap = new Label
-            {
-                HorizontalOptions = LayoutOptions.CenterAndExpand,
-                FontSize = 20,
-                Text = Characters[i, 0]
-            };
-
-            Label labelEng = new Label
-            {
-                HorizontalOptions = LayoutOptions.Center,
-                FontSize = 16,
-                Text = Characters[i, 1]
-            };
-
-            WewnetrznyLayout.Add(labelJap);
-            WewnetrznyLayout.Add(labelEng);
-
-            Border border = new Border()
-            {
-                WidthRequest = 80,
-                HeightRequest = 80,
-                Margin = 5,
-                StrokeThickness = 2,
-                Stroke = new SolidColorBrush(Color.FromRgb(211, 211, 211)),
-                StrokeShape = new RoundRectangle { CornerRadius = new CornerRadius(5) }
-            };
-
-            border.Content = WewnetrznyLayout;
-            stackLastRow.Add(border);
-        }
-        LayoutOfCharacters.Add(stackLastRow);
+     //dokonczyc ostatnie znaki w tablicy
     }
 }
--- Centrum/Pages/SettingsPage.xaml.cs
+++ Centrum/Pages/SettingsPage.xaml.cs
@@ -9,6 +9,7 @@ public partial class SettingsPage : ContentPage
 		InitializeComponent();
 		string choosenCapital = Preferences.Default.Get("Location", "Warsaw");
 		LocationPicker.SelectedIndex = ListOfCapitals.IndexOf(choosenCapital);
+		LearningLanguagePicker.SelectedItem = Preferences.Default.Get("LearningLanguage", "Japoñski");
 		LabelLocationChanged.IsVisible = false;
 	}
 
@@ -16,5 +17,15 @@ public partial class SettingsPage : ContentPage
     {
 		Preferences.Default.Set("Location",ListOfCapitals[LocationPicker.SelectedIndex]);
 		LabelLocationChanged.IsVisible = true;
+    }
+
+    private void ChangedLearningLanguage(object sender, EventArgs e)
+    {
+		var picker = (Picker)sender;
+		if (picker.SelectedIndex != -1)
+		{
+			string lng=(string)picker.SelectedItem;
+			Preferences.Default.Set("LearningLanguage",lng);
+		}
     }
 }
